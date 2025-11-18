0449d3dbd1ec8eef85559ef7ffe964e48a4f4365
TrungDong-PH52925 <dongntph52925@gmail.com>
2025-11-18 07:52:20 +0700

    Push day 3 - crfs

---
+++ database/migrations/2025_11_17_082720_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->bigInteger('amount')->unsigned()->nullable();
+            $table->enum('type',['income','expense']);
+            $table->string('description')->nullable();
+            $table->timestamp('date')->useCurrent()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Expense Tracker</title>
+    <style>
+        h1{
+            padding: 10px;
+            background: orange;
+            display: block;
+            align-content: center;
+            justify-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Expense Tracker - Website help you control your expense !</h1>
+    </div>
+</body>
+</html>
